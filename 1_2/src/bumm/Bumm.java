e899c7575f63e95530050a6e3b763a286aa356c2
csont2 <csont2@lab104-02.iit.uni-miskolc.hu>
2018-02-20 16:44:47 +0100

    1

---
+++ 1_2/src/bumm/Bumm.java
@@ -0,0 +1,19 @@
+package bumm;
+
+public class Bumm {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		for(int i=10;i>0;i--){
+			
+			System.out.println(i);
+			
+		}
+		
+		System.out.println("Bumm!");
+	}
+
+}
