166f9cb7b93fd7d7d7bddab1312814fae2422d33
csont2 <csont2@lab104-12.iit.uni-miskolc.hu>
2018-04-17 17:31:07 +0200

    9. gyak 1

---
+++ 9_gyak/src/polynomial/LinearPolynomial.java
@@ -0,0 +1,44 @@
+package polynomial;
+
+import math.IPolynomial;
+
+public class LinearPolynomial implements IPolynomial {
+	
+	private double a;
+	private double b;
+	
+	public LinearPolynomial(double a, double b){
+		super();			//meghívja az ősosztálya ami az object konstrukturát
+		this.a = a;
+		this.b = b;
+	}
+
+	@Override
+	public double getY(double x){
+		return this.a * x + this.b;
+	}
+	
+	@Override
+	public int getGrade(){
+		return 1;
+	}
+	
+	@Override
+	public String toString(){
+		if(this.b > 0)
+		return "y = " + this.a + " * x " + "+ " + this.b;
+		else if(this.b < 0)
+			return "y = " + this.a + " * x " + " - " + this.b;
+		else if(this.b == 0)
+			return "y = " + this.a + " * x " + " - " + 0;
+		
+		return "";
+	
+	
+	
+	}
+	
+	
+	
+	
+}
